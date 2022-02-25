dff055cfa2ff09812682d11a543db4841e8c35c6
johnsdjustin <johnsdjustin@gmail.com>
2022-02-24 23:10:44 -0600

    Refactored error formatting

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ import (
 // Param(s): A string representing the url to send the request to.
 // Return(s): A []byte representing the reponse body of the request in JSON, 
 // 			  the HTTP status code and an error.
-func fetch(url string) ([]byte, int, error){
+func Fetch(url string) ([]byte, int, error){
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, 500, err
@@ -39,12 +39,13 @@ func fetch(url string) ([]byte, int, error){
 // 			 CharacterSheet to be created
 // Return(s): A []byte representing the reponse body of the request in JSON, 
 // 			  the HTTP status code and an error.
-func create(url string, contentType string, data models.CharacterSheet) ([]byte, int, error) {
+func Create(url string, contentType string, data models.CharacterSheet) ([]byte, int, error) {
 
 	json, err := json.Marshal(data)
 
 	if err != nil {
 		log.Fatalln(err)
+		return nil, 500, err
 	}
 
 	reqBody := bytes.NewBuffer(json)
@@ -52,30 +53,71 @@ func create(url string, contentType string, data models.CharacterSheet) ([]byte,
 	resp, err := http.Post(url, contentType, reqBody)
 	if err != nil {
 		log.Fatalln(err)
+		return nil, 500, err
 	}
 	defer resp.Body.Close()
 
 	respBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
+		return nil, 500, err
 	}
 
 	return respBody, resp.StatusCode, nil
 }
 
 
+type ErrorResponse struct {
+	Message string `json:"message"`
+}
+
+func MapErrorResponse(code int) string {
+
+	var errorRespString string
+
+	// TODO: Map more status codes to strings as needed
+	switch {
+		case code == http.StatusNotFound:
+			errorRespString = "Resource Not Found"
+		case code == http.StatusBadRequest:
+			errorRespString = "Bad Request"
+		case code >= 500:
+			errorRespString = "Server Error"
+		default:
+			errorRespString = "Oops! Something went wrong. Please try again!"
+	}
+
+	return errorRespString
+}
+
+
+
+func GenerateError(code int) ErrorResponse {
+	errorMessage := MapErrorResponse(code)
+	errorResponse := ErrorResponse{
+		Message: errorMessage,
+	}
+	// errorRespMap := map[string]string{
+	// 	"Message": errorMessage,
+	// }
+
+	return errorResponse
+}
+
 func getCharacters(c *gin.Context){
 	var characters []models.CharacterSheet
-	url := "https://my-json-server.typicode.com/johnsdjustin/character-sheets-db/characters"
+	url := "https://my-json-server.typicode.com/johnsdjustin/character-sheets-db/charactersAAA"
 
-	body, code, err := fetch(url)
-	if err != nil || code >= 500 {
-		log.Fatalln(err)
+	body, code, err := Fetch(url)
+	if err != nil || code >= 400 {
+		c.JSON(code, GenerateError(code))
+		return
 	}
 
 	jErr := json.Unmarshal(body, &characters)
 	if jErr != nil {
-		log.Fatalln(err)
+		c.JSON(500, GenerateError(500))
+		return
 	}
 
 	c.JSON(http.StatusOK, characters)
@@ -87,19 +129,16 @@ func getCharactersById(c *gin.Context){
 	resourceUrl := fmt.Sprintf("%s/%s", baseUrl, id)
 	var character models.CharacterSheet
 
-	body, code, err := fetch(resourceUrl)
-	if err != nil || code >= 500 {
-		log.Fatalln(err)
-	}
-
-	if code == 404 {
-		c.JSON(code, gin.H{"Message": "Not Found"})
+	body, code, err := Fetch(resourceUrl)
+	if err != nil {
+		c.JSON(code, GenerateError(code))
 		return
 	}
 
 	jErr := json.Unmarshal(body, &character)
 	if jErr != nil {
-		log.Fatalln(jErr)
+		c.JSON(500, GenerateError(500))
+		return
 	}
 
 	c.JSON(http.StatusFound, character)
@@ -113,17 +152,18 @@ func postCharacters(c *gin.Context){
 
 	err := c.BindJSON(&newCharacter)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "Interal failure - please try again"})
+		c.JSON(500, GenerateError(500))
 		return
 	}
 
-	body, code, err := create(url, contentType, newCharacter)
+	body, code, err := Create(url, contentType, newCharacter)
 	if err != nil {
-		log.Fatalln(err)
+		c.JSON(code, GenerateError(code))
+		return
 	}
 
 	if code != http.StatusCreated {
-		c.JSON(http.StatusCreated, gin.H{"message": "Character creation failed. Please try again"})
+		c.JSON(500, GenerateError(500))
 		return
 	}
 
