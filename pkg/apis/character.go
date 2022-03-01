73292671bd0870c5ffb85bc314207ac8549f95ae
johnsdjustin <johnsdjustin@gmail.com>
2022-02-28 20:59:53 -0600

    Created API

---
+++ pkg/apis/character.go
@@ -0,0 +1,60 @@
+package apis
+
+import (
+	"net/http"
+	"github.com/dnd-character-api/pkg/models"
+	"github.com/dnd-character-api/pkg/repositories"
+	"github.com/dnd-character-api/pkg/utils"
+	"github.com/gin-gonic/gin"
+)
+
+
+type CharacterAPI struct {
+	Repository repositories.Repository
+}
+
+func (API CharacterAPI) postCharacters(c *gin.Context){
+	var newCharacter models.CharacterSheet
+	repository := API.Repository
+
+	err := c.BindJSON(&newCharacter)
+	if err != nil {
+		c.JSON(500, utils.GenerateError(500))
+		return
+	}
+
+	created, createdCharacter := repository.CreateCharacter(newCharacter)
+
+	if !created {
+		c.JSON(400, utils.GenerateError(400))
+		return
+	}
+
+	c.JSON(http.StatusCreated, createdCharacter)
+}
+
+func (API CharacterAPI) getCharacters(c *gin.Context){
+
+	repository := API.Repository
+
+	characters := repository.GetAllCharacters()
+
+	c.JSON(http.StatusOK, characters)
+}
+
+func (API CharacterAPI) getCharactersById(c *gin.Context){
+	id := c.Param("id")
+	repository := API.Repository
+
+	character := repository.GetCharacter(id)
+
+	c.JSON(http.StatusFound, character)
+}
+
+func (API CharacterAPI) Start(address string) {
+	router := gin.Default(); 
+	router.GET("/characters", API.getCharacters)
+	router.GET("/characters/:id", API.getCharactersById)
+	router.POST("characters", API.postCharacters)
+	router.Run(address); 
+}
